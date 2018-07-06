0ce734498ff20bfcd1f6a9e87d0627ec818a3944
PhilipJFryy <ch.wunderlich@freenet.de>
2018-07-06 14:23:22 +0200

    FIrst Upload v.1.7

---
+++ @slz/addons/sl_zeus/config.cpp
@@ -0,0 +1,14 @@
+#include "hpp\macros.hpp"
+
+class CfgPatches {
+	class SL_Zeus {
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {};
+		author[]= {"seelenlos"};		
+	};		
+};
+
+class CfgFunctions {
+	#include "hpp\functions.hpp"
+};
---
+++ @slz/addons/sl_zeus/hpp/functions.hpp
@@ -0,0 +1,16 @@
+#include "macros.hpp"
+
+class TAG {
+	class functions {		
+		file = "sl_zeus\functions";		
+		
+		class addObjs {};
+		class initClient {};
+		class initPost {postInit = 1;};
+		class getCuratorObj {};
+		class initPre {preInit = 1;};
+		class notification {};
+		class assignCurator {};
+		class initWatchDog {};
+	};
+};
